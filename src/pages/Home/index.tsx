24bf9cc160750fd70e2d926969fb2f8df969144b
SirZemar <eduardo.w.marinho@gmail.com>
2023-08-07 10:44:36 +0100

    setup app component with home page and global styles

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { HomeContainer } from "./Home.styles";
+
+const Home: React.FC = () => {
+  return <HomeContainer>Hello from home component</HomeContainer>;
+};
+
+export default Home;
