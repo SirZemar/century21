e6879b61e15b261eddebe93f67825c61c029c6d7
SirZemar <eduardo.w.marinho@gmail.com>
2023-08-09 16:38:46 +0100

    add temporary translate file

---
+++ src/translate.ts
@@ -0,0 +1,20 @@
+export const translate = {
+  CONTACT: {
+    FORM: {
+      EMAIL: {
+        EMPTY: "Por favor preencha com um email",
+        INVALID: "Por favor preencha com um email válido",
+      },
+      PHONE: {
+        EMPTY: "Por favor preencha com um número de telemóvel",
+        INVALID: "Por favor preencha com um número de telemóvel válido",
+      },
+      NAME: {
+        EMPTY: "Por favor preencha com o seu nome",
+      },
+      MESSAGE: {
+        EMPTY: "Por favor preencha com a sua mensagem",
+      },
+    },
+  },
+};
