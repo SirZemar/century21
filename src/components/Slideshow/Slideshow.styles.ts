bac758d4ce09cbf40807ec87f26fd864b07fca88
SirZemar <eduardo.w.marinho@gmail.com>
2023-08-09 18:06:40 +0100

    feat(slideshow): slide transitin changes when click arrows

--- src/components/Slideshow/Slideshow.styles.ts
+++ src/components/Slideshow/Slideshow.styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { heightNoNavbar } from "../../utils.styles";
 
-export const SlideshowContainer = styled.div`
+export const SlideshowContainer = styled.div<any>`
   .slideshow {
     max-width: 100%;
     &__slide {
@@ -10,6 +10,25 @@ export const SlideshowContainer = styled.div`
       background-repeat: no-repeat;
       background-position: center;
       background-color: var(--primary);
+    }
+  }
+  .indicators {
+    margin-top: -20px !important;
+    li::before {
+      content: "";
+      display: block;
+      position: absolute;
+      left: 0;
+      top: 0;
+      padding: 5px;
+      opacity: 0.8;
+      background-color: var(--primary);
+    }
+
+    button::before {
+      transform: translate(1.5px, 1.5px);
+      background: var(--secondary) !important;
+      border: 1px outset white;
     }
   }
 `;
--- src/components/Slideshow/index.tsx
+++ src/components/Slideshow/index.tsx
@@ -1,16 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { SlideshowContainer } from "./Slideshow.styles";
 import { Slide } from "react-slideshow-image";
 
 const Slideshow: React.FC = () => {
+  const [slideSpeed, setSlideSpeed] = useState(1000);
   const images = [
     "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
     "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
     "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
   ];
+
+  const buttonStyle = {
+    width: "30px",
+    background: "none",
+    border: "0px",
+  };
+
+  const properties = {
+    prevArrow: (
+      <button style={{ ...buttonStyle }}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+          fill="#fff"
+        >
+          <path d="M242 180.6v-138L0 256l242 213.4V331.2h270V180.6z" />
+        </svg>
+      </button>
+    ),
+    nextArrow: (
+      <button style={{ ...buttonStyle }}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+          fill="#fff"
+        >
+          <path d="M512 256L270 42.6v138.2H0v150.6h270v138z" />
+        </svg>
+      </button>
+    ),
+  };
+
+  console.log(slideSpeed);
+
   return (
-    <SlideshowContainer>
-      <Slide cssClass="slideshow">
+    <SlideshowContainer
+      onMouseLeave={() => {
+        setSlideSpeed(1000);
+      }}
+      onMouseEnter={() => {
+        setSlideSpeed(100);
+      }}
+    >
+      <Slide
+        transitionDuration={slideSpeed}
+        indicators={true}
+        cssClass="slideshow"
+        {...properties}
+      >
         <div>
           <div
             className="slideshow__slide"
