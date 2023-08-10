af4f7da63e0cb18f55a08417248d5cddcab133e6
SirZemar <eduardo.w.marinho@gmail.com>
2023-08-10 10:52:16 +0100

    add customize messages

--- src/components/Contact/Contact.styles.ts
+++ src/components/Contact/Contact.styles.ts
@@ -1,3 +1,17 @@
 import styled from "styled-components";
+import { device } from "../../devices";
 
-export const ContactContainer = styled.div``;
+export const ContactContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+
+  && .form {
+    max-width: 80%;
+    width: 100%;
+
+    @media ${device.tablet} {
+      max-width: 100%;
+    }
+  }
+`;
