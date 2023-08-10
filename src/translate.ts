8277adac60b01b9bfdf480e07bf5d049b462649b
SirZemar <eduardo.w.marinho@gmail.com>
2023-08-10 17:42:24 +0100

    add contact form translate fields

--- src/translate.ts
+++ src/translate.ts
@@ -4,17 +4,32 @@ export const translate = {
       EMAIL: {
         EMPTY: "Por favor preencha com um email",
         INVALID: "Por favor preencha com um email válido",
+        LABEL: "Email",
+        PLACEHOLDER: "Email...",
       },
       PHONE: {
         EMPTY: "Por favor preencha com um número de telemóvel",
         INVALID: "Por favor preencha com um número de telemóvel válido",
+        LABEL: "Telemóvel",
+        PLACEHOLDER: "Telemóvel...",
       },
       NAME: {
         EMPTY: "Por favor preencha com o seu nome",
+        LABEL: "Nome",
+        PLACEHOLDER: "Nome...",
       },
       MESSAGE: {
         EMPTY: "Por favor preencha com a sua mensagem",
+        LABEL: "Mensagem",
+        PLACEHOLDER: "Mensagem...",
       },
+      BUTTONS: {
+        SUBMIT: "Submeter",
+      },
+    },
+    EMAIL_SERVICE: {
+      SENT: "O seu email foi enviado com sucesso",
+      ERROR: "Ups, não foi possível enviar o seu email",
     },
   },
 };
