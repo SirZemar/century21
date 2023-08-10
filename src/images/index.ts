1e02ae077c015c59339476094ba78d2325afe5ce
SirZemar <eduardo.w.marinho@gmail.com>
2023-08-10 10:52:53 +0100

    add images

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,5 +1,6 @@
 import HeroHouse from "./hero-house.jpg";
 import Aerial1 from "./aerea-1.jpg";
 import Balcony from "./balcony.jpg";
+import Map from "./location.png";
 
-export { HeroHouse, Aerial1, Balcony };
+export { HeroHouse, Aerial1, Balcony, Map };
