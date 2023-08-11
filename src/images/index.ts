3a44ffe205e266f77eb1bfbd7151bd93b7d9feeb
SirZemar <eduardo.w.marinho@gmail.com>
2023-08-11 17:07:04 +0100

    add title decorator image

--- src/images/index.ts
+++ src/images/index.ts
@@ -2,5 +2,6 @@ import HeroHouse from "./hero-house.jpg";
 import Aerial1 from "./aerea-1.jpg";
 import Balcony from "./balcony.jpg";
 import Map from "./location.png";
+import TitleLine from "./design/title-line.png";
 
-export { HeroHouse, Aerial1, Balcony, Map };
+export { HeroHouse, Aerial1, Balcony, Map, TitleLine };
