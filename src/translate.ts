c4cd713f121b466f16b9babc29c382363115df36
SirZemar <eduardo.w.marinho@gmail.com>
2023-08-11 17:05:05 +0100

    add translations to project page

--- src/translate.ts
+++ src/translate.ts
@@ -1,5 +1,25 @@
 export const translate = {
+  OVERVIEW: {
+    HEADER: "Vista Geral",
+    TITLE: "Apartamentos com vista para o rio",
+    TEXT: "Apartamentos de luxo localizados numa das melhores zonas da cidade. Todos os apartamentos têm uma vista de excelência e uma varanda na qual pode apreciar a beleza interminável do rio Douro.",
+  },
+  APARTMENTS: {
+    HEADER: "Apartamentos",
+    TITLE: "Cada apartamento é especial!",
+    TEXT: "Explore nossas plantas: descubra espaços bem projetados e layouts inteligentes para seu novo lar perfeito.",
+  },
+  LOCATION: {
+    HEADER: "Localização",
+    TITLE: "Tudo o que tu precisas",
+    TEXT: {
+      ITEM1: "Supermercado Pingo Doce - 200m",
+      ITEM2: "Terminal Intermodal de Campanhã - 400m",
+      ITEM3: "Alameda das Fontaínhas - 450m",
+    },
+  },
   CONTACT: {
+    HEADER: "Contacta-nos",
     FORM: {
       EMAIL: {
         EMPTY: "Por favor preencha com um email",
@@ -24,7 +44,7 @@ export const translate = {
         PLACEHOLDER: "Mensagem...",
       },
       BUTTONS: {
-        SUBMIT: "Submeter",
+        SUBMIT: "Enviar",
       },
     },
     EMAIL_SERVICE: {
