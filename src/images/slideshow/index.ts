6c2d3a8d86b23bf6499c1742e7b91802c18dff9c
SirZemar <eduardo.w.marinho@gmail.com>
2023-08-11 11:17:48 +0100

    temporary smaller images

---
+++ src/images/slideshow/index.ts
@@ -0,0 +1,21 @@
+import image11 from "./1.jpg";
+import image22 from "./2.jpg";
+import image33 from "./3.jpg";
+import image44 from "./4.jpg";
+import image55 from "./5.jpg";
+import image66 from "./6.jpg";
+import image77 from "./7.jpg";
+import image88 from "./8.jpg";
+import image99 from "./9.jpg";
+
+export const slideshowImages = [
+  image11,
+  image22,
+  image33,
+  image44,
+  image55,
+  image66,
+  image77,
+  image88,
+  image99,
+];
